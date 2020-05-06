cc89dddece0382504d39e46dc780d5c27a370eaf
Gio <giovanni.berl1@gmail.com>
2020-05-06 23:55:36 +0200

    aggiunto timeout visualizzazione footer-top, cambiato riferimento di position absolute impostando relative sulla lista a destra anziche sul icona menu e aggiunto document ready

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-//shadow sull'hover del search
-$(".search").mouseenter(function () {
-    $(this).addClass("shadow");
-});
-$(".search").mouseleave(function () {
-    $(this).removeClass("shadow");
-});
-// --------------------
+$(document).ready(function () {
+    //imposto visualizzazione del footer-top dopo 3 secondi
+    setTimeout(function () {
+        $(".footer-top-wrapper").fadeIn();
+    }, 3000);
 
-//comparsa-scomparsa dropdown al click sull'icona
-$(".menu-icon").click(function () {
-    $(".dropdown").toggle();
-});
+    //shadow sull'hover del search
+    $(".search").mouseenter(function () {
+        $(this).addClass("shadow");
+    });
+    $(".search").mouseleave(function () {
+        $(this).removeClass("shadow");
+    });
+    // --------------------
 
-// !RICORDA CHE DEVI SELEZIONARE IL TARGET COME OGGETTO JQUERY ==> $(e.target) senno se usi solo e.target e' un oggetto JS a cui non puoi applicare i metodi di JQUERY
-$(document).click(function (e) {
-    var target = $(e.target);
-    if ($(".dropdown").is(":visible") && !target.hasClass("menu-icon")) {
-        $("div.dropdown").hide();
-    }
+    //comparsa-scomparsa dropdown al click sull'icona
+    $(".menu-icon").click(function () {
+        $(".dropdown").toggle();
+    });
+
+    //click sul document che mi nasconde il dropdown (se aperto)
+    // !RICORDA CHE DEVI SELEZIONARE IL TARGET COME OGGETTO JQUERY ==> $(e.target) senno se usi solo e.target e' un oggetto JS a cui non puoi applicare i metodi di JQUERY
+    $(document).click(function (e) {
+        var target = $(e.target);
+        if ($(".dropdown").is(":visible") && !target.hasClass("menu-icon")) {
+            $("div.dropdown").hide();
+        }
+    });
 });
