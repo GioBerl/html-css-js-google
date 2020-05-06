f5a26dce926a24867bcb8a40e2e57dff245e306b
Gio <giovanni.berl1@gmail.com>
2020-05-06 09:23:35 +0200

    sembra ok

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+//shadow sull'hover del search
 $(".search").mouseenter(function () {
     $(this).addClass("shadow");
 });
 $(".search").mouseleave(function () {
     $(this).removeClass("shadow");
 });
+// --------------------
 
+//comparsa-scomparsa dropdown al click sull'icona
 $(".menu-icon").click(function () {
     $(".dropdown").toggle();
 });
